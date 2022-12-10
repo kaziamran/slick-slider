031b3b4a2a623b3d676690eea5ff61cf1f0cc8d0
kazi Emran <kaziamran4@gmail.com>
2022-12-10 11:34:42 +0600

    add slider padding

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,8 @@
 
 $('._project_slick_slider').slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    // variableWidth: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     prevArrow: '<button class="slide-arrow prev-arrow prev-arrow1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z"/></svg></button>',
@@ -10,12 +11,16 @@ $('._project_slick_slider').slick({
 
 
 $('._project_slick_center_slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
     centerMode: true,
-    centerPadding: '60px',
+    centerPadding: '0',
     slidesToShow: 3,
     slidesToScroll: 1,
     cssEase: 'ease',
     swipe: 'true',
+    draggable: true,
+    dots: true,
     prevArrow: '<button class="slide-arrow prev-arrow prev-arrow2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 2c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zm0 18c4.42 0 8-3.58 8-8s-3.58-8-8-8-8 3.58-8 8 3.58 8 8 8zm0-9h4v2h-4v3l-4-4 4-4v3z"/></svg></button>',
     nextArrow: '<button class="slide-arrow next-arrow next-arrow2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 11V8l4 4-4 4v-3H8v-2h4zm0-9c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zm0 18c4.42 0 8-3.58 8-8s-3.58-8-8-8-8 3.58-8 8 3.58 8 8 8z"/></svg></button>'
 });
